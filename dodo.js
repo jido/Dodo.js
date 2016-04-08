43b1d83533034e200bc9f56d13672a6df37eac7f
jido <denisgithub@bredelet.com>
2016-04-08 07:40:52 +0100

    Redesign of dodo.js
    Replace copy with the two functions copy and extend. Add type checking.

--- dodo.js
+++ dodo.js
@@ -2,35 +2,56 @@
 
 var dodo = {};
 
-if ("console" in window && "log" in console) {
-	dodo.log = function() {
-		console.log.apply(console, arguments);
-	};
-	dodo.dumpLog = function() {};
+if ("console" in window && "log" in console) 
+{
+    dodo.log = function() {
+        console.log.apply(console, arguments);
+    };
+    dodo.dumpLog = function() {};
 }
-else {
-	dodo.logMessages = "";
-	dodo.log = function() {
-		var line = "";
-		var len = arguments.length;
-		for (var i = 0; i < len; i++) {
-			line += arguments[i];
-		}
-		dodo.logMessages += line + "\n";
-	};
-	dodo.dumpLog = function() {
-		throw new Error(dodo.logMessages);
-	};
+else 
+{
+    dodo.logMessages = "";
+    dodo.log = function() {
+        var line = "";
+        var len = arguments.length;
+        for (var i = 0; i < len; i++) 
+        {
+            line += arguments[i];
+        }
+        dodo.logMessages += line + "\n";
+    };
+    dodo.dumpLog = function() {
+        throw new Error(dodo.logMessages);
+    };
 }
+
+dodo.toString = function() {
+    var desc = "{";
+    for (var attr in this) 
+    {
+        var val = this[attr];
+        if (typeof val === "function") 
+        {
+            if (!this.hasOwnProperty(attr)) continue;
+            val = "function()";
+        }
+        desc += attr + ": " + val + ",";
+    }
+    return desc.replace(/,?$/, "}");
+};
+
 
 dodo.log("Loading dodo object model");
 
-if ("create" in Object) {
-	dodo.create = function(o) {
-		return Object.create(o);
-	};
+if ("create" in Object) 
+{
+    dodo.create = function(o) {
+        return Object.create(o);
+    };
 }
-else {
+else 
+{
     dodo.create = function (o) {
         function F() {}
         F.prototype = o;
@@ -39,178 +60,128 @@ else {
 }
 
 dodo.definePropertyFailback = function (o, name, descriptor) {
-	if ("value" in descriptor) {
-		o[name] = descriptor.value;
-	}
-	else if ("get" in descriptor || "set" in descriptor) {
-		throw new Error("dodo._defineProperty implementation only accepts data descriptors: property: " + name);
-	}
-	// ignore other cases
+    if ("value" in descriptor) 
+    {
+        o[name] = descriptor.value;
+    }
+    else if ("get" in descriptor || "set" in descriptor) 
+    {
+        throw new Error("dodo._defineProperty implementation only accepts data descriptors: property: " + name);
+    }
+    // ignore other cases
 };
 
 dodo.canDefineProperty = false;
 
-if ("defineProperty" in Object) {
-	try {
-		// The following fails in IE8
-		Object.defineProperty({}, "dummy", {value: 1, writable: true});
-		dodo.canDefineProperty = true;
-	}
-	catch (someException) {
-		dodo.log("Object.defineProperty defined, but fails on non-DOM objects!");
-	}
+if ("defineProperty" in Object) 
+{
+    try {
+        // The following fails in IE8
+        Object.defineProperty({}, "dummy", {value: 1, writable: true});
+        dodo.canDefineProperty = true;
+    }
+    catch (someException) {
+        dodo.log("Object.defineProperty defined, but fails on non-DOM objects!");
+    }
 }
 
-if (dodo.canDefineProperty) {
-	dodo._defineProperty = function(o, name, descriptor) {
-		try {
-			Object.defineProperty(o, name, descriptor);
-		}
-		catch (someException) {
-			dodo.log("dodo._defineProperty: ", someException);
-			dodo.definePropertyFailback(o, name, descriptor);
-		}
-	};
+if (dodo.canDefineProperty) 
+{
+    dodo._defineProperty = function(o, name, descriptor) {
+        try {
+            Object.defineProperty(o, name, descriptor);
+        }
+        catch (someException) {
+            dodo.log("dodo._defineProperty: ", someException);
+            dodo.definePropertyFailback(o, name, descriptor);
+        }
+    };
 }
-else {
-	dodo._defineProperty = dodo.definePropertyFailback;
-	dodo.log("Define object property not available!");
+else
+{
+    dodo._defineProperty = dodo.definePropertyFailback;
+    dodo.log("Define object property not available!");
 }
 
+dodo.objectType = (function(global) {
+    return function type(value) {
+        if (value === global)
+        {
+            return "global";
+        }
+        else
+        {
+                return Object.prototype.toString.call(value);
+        }
+    };
+})(this);
 
-dodo.defaultConstructor = function(constructorList, args) {
-	var constructor = constructorList[args.length];
-	if (typeof constructor === "undefined") {
-		throw new Error("dodo.defaultConstructor: No constructor with arity: " + args.length);
-	}
-	constructor.apply(this, args);
+dodo.checkType = function(variable, value, reference) {
+    if (typeof value !== typeof reference ||
+            (value instanceof Object && !(value instanceof reference.constructor)))
+    {
+        throw new Error("checkType: incompatible type: " + variable + " is " + 
+                dodo.objectType(value) + ", expected: " + dodo.objectType(reference));
+    }
 };
 
-dodo.copy = function(proto) {
-//	dodo.log("in copy: proto: " + proto);
-	var superProto = this.prototype;
-	var self = dodo.create(proto);
-	var field;
-	var i = 2;
-	while (i < arguments.length) {
-		field = arguments[i-1];
-//		dodo.log("in copy: setting field: " + field);
-		if (!(field in superProto)) {
-			throw new Error("copy: unknown field: " + field);
-		}
-		if (typeof self[field] === "function") {
-			throw new Error("copy: not an attribute of object: " + field);
-		}
-		dodo._defineProperty(self, field, {value: arguments[i], writable: false, enumerable: true});
-		i += 2;
-	}
-	if (i === arguments.length) {
-		var extension = arguments[i-1];
-		for (field in extension) {
-//			dodo.log("in copy: adding field: " + field);
-			if (field in superProto) {
-				throw new Error("copy: field already exists: " + field);
-			}
-			var value = extension[field];
-			if (field.lastIndexOf("$", 0) === 0)
-			{
-				field = field.substr(1);
-				if (!(field in superProto)) {
-					throw new Error("copy: cannot override unknown field: " + field);
-				}
-			}
-			dodo._defineProperty(self, field, {value: value, writable: false, enumerable: true});
-		}
-	}
-	if (typeof proto === "function") {
-		// Assume constructor
-		var MakeInstance = function() {
-			proto.apply(this, arguments);
-		};
-		MakeInstance.prototype = self.prototype;
-		MakeInstance.constructor = self.constructor;
-		return MakeInstance;
-	}
-	return self;
+function copy(proto, changes) {
+    var self = dodo.create(proto);
+    for (var attribute in changes)
+    {
+        if (!(attribute in proto))
+        {
+            throw new Error("copy: unknown attribute: " + attribute);
+        }
+        var value = changes[attribute];
+        dodo.checkType(attribute, value, proto[attribute]);
+        dodo._defineProperty(self, attribute, {value: value, writable: false, enumerable: true});
+    }
+    return self;
 }
 
-function copy(proto) {
-    var Super = proto.constructor;
-    return dodo.copy.apply(Super, arguments);
+function extend(proto, extension) {
+    var self = dodo.create(proto);
+    var extended = false;
+    for (var attribute in extension) 
+    {
+        var value = extension[attribute];
+        if (attribute.lastIndexOf("$", 0) === 0)
+        {
+            attribute = attribute.substr(1);
+            if (!(attribute in proto)) 
+            {
+                throw new Error("extend: cannot override unknown attribute: " + attribute);
+            }
+            dodo.checkType(attribute, value, proto[attribute]);
+        }
+        else if (attribute in proto)
+        {
+            throw new Error("extend: attribute already exists: " + attribute);
+        }
+        else 
+        {
+            extended = true;
+        }
+        dodo._defineProperty(self, attribute, {value: value, writable: false, enumerable: true});
+    }
+    if (extended)
+    {
+        if (self.toString !== dodo.toString)
+        {
+            self.toString = dodo.toString;
+        }
+        var MakeInstance = function() {
+            return copy(self);
+        };
+        dodo._defineProperty(self, 'constructor', {value: MakeInstance, writable: false, enumerable: false});
+        MakeInstance.prototype = proto;
+        MakeInstance.instance = self;
+    }
+    return self;
 }
 
-function Default() {
-	dodo._defineProperty(this, "constructor", {value: Default, writable: true});
-}
-
-Default.prototype = {
-	toString: function() {
-		var desc = "{";
-		for (var attr in this) {
-			var val = this[attr];
-			if (typeof val === "function") {
-				if (!this.hasOwnProperty(attr)) continue;
-				val = "function()";
-			}
-			desc += attr + ": " + val + ",";
-		}
-		return desc.replace(/,?$/, "}");
-	}
-};
-
-var struct = new Default();
-
-function Class(declarations) {
-	// Create the prototype of the objects of the class
-	var proto = dodo.copy.call(this, this.prototype, declarations);
-
-	// Class constructor
-	var MakeInstance;
-	if (arguments.length < 2) {
-		// Inherit parent constructors
-		var Super = this.constructor.prototype;
-	}
-	else {
-		// New list of constructors
-		var constructorList = arguments[1];
-	}
-	function MakeInstanceInherited() {
-		// call super
-		Super.apply(this, arguments);
-		dodo._defineProperty(this, "constructor", {value: MakeInstanceInherited, writable: true});
-	}
-	function MakeInstanceWithConstructors() {
-		dodo.defaultConstructor.call(this, constructorList, arguments);
-		dodo._defineProperty(this, "constructor", {value: MakeInstanceWithConstructors, writable: true});
-	}
-	MakeInstance = (arguments.length < 2? MakeInstanceInherited: MakeInstanceWithConstructors);
-	MakeInstance.prototype = proto;
-
-	// Metaclass constructor (for extending the class)
-	function MakeClass() {
-		return Class.apply(this, arguments);
-	}
-	MakeClass.prototype = MakeInstance;
-	MakeInstance.constructor = MakeClass;
-
-	return MakeInstance;
-}
-
-Class.prototype = Default;
-
-
-function Link(T) {
-	this.$ = (typeof T === "function"? new T(): "");
-}
-
-Link.prototype = {
-	toString: function() {
-		return (typeof this.$ === "object"? "Link()": this.$);
-	}
-};
-
-function linkTo(val) {
-	var ln = new Link();
-	ln.$ = val;
-	return ln;
-}
+var x = extend({}, {a:3, z:true, b:{}});
+alert(copy(x, {a:-5.6}));
+var y = extend(x, {c:"hi", $a:9});
+copy(copy(x, {b:y}), {b:x});
